a320c93d4623a3d12d8beacd1e7815d40dd6175b
leetwinoff <leetwinoff92@gmail.com>
2023-07-20 12:14:18 -0500

    Added CrewSerializer with create method

--- airport/serializers.py
+++ airport/serializers.py
@@ -16,3 +16,16 @@ class CrewPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = CrewPosition
         fields = ("position",)
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    position = serializers.PrimaryKeyRelatedField(queryset=CrewPosition.objects.all())
+
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name", "position")
+
+    def create(self, validated_data):
+        position_id = validated_data.pop("position")
+        crew = Crew.objects.create(position=position_id, **validated_data)
+        return crew
