c5cbdddee613ea60d3ff60941361b20c7693ce87
leetwinoff <leetwinoff92@gmail.com>
2023-07-20 11:35:03 -0500

    Added Crew Model

--- airport/models.py
+++ airport/models.py
@@ -6,3 +6,12 @@ class CrewPosition(models.Model):
 
     def __str__(self):
         return self.position
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.ForeignKey(CrewPosition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
