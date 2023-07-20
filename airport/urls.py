55058ae231bcd1691a4d4f1780fee2cffa08b5e8
leetwinoff <leetwinoff92@gmail.com>
2023-07-20 12:33:27 -0500

    Added router for AirportViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from airport.views import CrewPositionViewSet, CrewViewSet
+from airport.views import CrewPositionViewSet, CrewViewSet, AirportViewSet
 
 router = DefaultRouter()
 router.register("crew_position", CrewPositionViewSet)
 router.register("crew", CrewViewSet)
+router.register("", AirportViewSet)
 
 
 
