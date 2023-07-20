18f0a19661117871a24d4563506fbbdfeab14917
leetwinoff <leetwinoff92@gmail.com>
2023-07-20 12:26:18 -0500

    Added CrewViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from airport.models import CrewPosition
-from airport.serializers import CrewPositionSerializer
+from airport.models import CrewPosition, Crew
+from airport.serializers import CrewPositionSerializer, CrewSerializer
 
 
 class CrewPositionViewSet(viewsets.ModelViewSet):
     queryset = CrewPosition.objects.all()
     serializer_class = CrewPositionSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
