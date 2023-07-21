478fb5e86da66e1fe8c6da114bbb8d65bb57e00b
leetwinoff <leetwinoff92@gmail.com>
2023-07-21 08:26:43 -0500

    Added CreateUserView, ManagerView and logoutView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class LogOutView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        token = get_object_or_404(Token, user=request.user)
+        token.delete()
+        return Response({"detail": "Succesfully log out"}, status=status.HTTP_200_OK)
