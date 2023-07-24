2ec53fca26d2baa428c7e5d1115b1ccca0e7f9b2
leetwinoff <leetwinoff92@gmail.com>
2023-07-24 15:05:31 -0500

    Added endpoints for TicketViewSet and OrderViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
 from airport.views import CrewPositionViewSet, CrewViewSet, AirportViewSet, RouteViewSet, AirplaneViewSet, \
-    FlightViewSet, AirplaneTypeViewSet
+    FlightViewSet, AirplaneTypeViewSet, TicketViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register("crew_position", CrewPositionViewSet)
@@ -11,6 +11,8 @@ router.register("route", RouteViewSet)
 router.register("airplane_type", AirplaneTypeViewSet)
 router.register("airplane", AirplaneViewSet)
 router.register("flight", FlightViewSet)
+router.register("tickets", TicketViewSet)
+router.register("order", OrderViewSet)
 
 
 
